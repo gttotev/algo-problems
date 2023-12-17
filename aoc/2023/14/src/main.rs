e453e2ba93512de0621b01c3034bfef6b949ea62
Gabriel Totev <gttotev8@gmail.com>
2023-12-16 22:43:29 -0800

    Solve AoC 2023 Day 14 Part 2

--- aoc/2023/14/src/main.rs
+++ aoc/2023/14/src/main.rs
@@ -1,4 +1,4 @@
-fn part1() -> u64 {
+fn _part1() -> u64 {
     let mut os = 0;
     let mut sub = 0;
     let mut rows = 0;
@@ -19,6 +19,60 @@ fn part1() -> u64 {
     rows * os - sub
 }
 
+fn tilt(grid: &mut Vec<Vec<u8>>, ns: bool, nw: bool) -> u64 {
+    let (rows, cols) = (grid.len(), grid[0].len());
+    let (il, jl) = if nw { (0, 0) } else { (rows-1, cols-1) };
+    let mut res = 0;
+    let mut coli = [0usize; 128];
+    for i in 0..rows {
+        let ii = il.abs_diff(i);
+        let mut rowj = 0usize;
+        for j in 0..cols {
+            let jj = jl.abs_diff(j);
+            let c = grid[ii][jj];
+            if c == b'O' {
+                grid[ii][jj] = b'.';
+                res += i + 1;
+                if ns {
+                    grid[coli[j].abs_diff(il)][jj] = b'O';
+                } else {
+                    grid[ii][rowj.abs_diff(jl)] = b'O';
+                }
+                coli[j] += 1;
+                rowj += 1;
+            } else if c == b'#' {
+                coli[j] = i + 1;
+                rowj = j + 1;
+            }
+        }
+    }
+    res as u64
+}
+
+fn cycle(grid: &mut Vec<Vec<u8>>) {
+    tilt(grid, true, true);
+    tilt(grid, false, true);
+    tilt(grid, true, false);
+    tilt(grid, false, false);
+}
+
+fn part2() -> u64 {
+    const CYCLES: usize = 1000000000;
+    let mut history = Vec::new();
+    let mut grid: Vec<_> = std::io::stdin().lines().flatten().map(|s| s.into_bytes()).collect();
+    for i in 0..CYCLES {
+        cycle(&mut grid);
+        for j in (0..i).rev() {
+            if history[j] == grid {
+                return tilt(&mut history[j + (CYCLES-j-1) % (i-j)], false, false);
+            }
+        }
+        history.push(grid.clone());
+    }
+    0
+}
+
 fn main() {
-    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
+    // println!("Part 1: {}", _part1());
 }
